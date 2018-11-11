4f5e821d44f43cbfbdfddd07fee51a030a10f9c8
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-11-11 21:07:29 +0900

    .

--- vue-trello/src/components/templates/TrelloLoginView.vue
+++ vue-trello/src/components/templates/TrelloLoginView.vue
@@ -1,3 +1,33 @@
 <template>
-  <p>ログインページ</p>
+    <div class="login-view">
+        <h1>Trello</h1>
+        <TrelloLoginForm></TrelloLoginForm>
+    </div>
 </template>
+
+<script>
+import TrelloLoginForm from "@/components/molecules/TrelloLoginForm";
+
+export default {
+  name: "TrelloLoginView",
+  components: {
+    TrelloLoginForm
+  },
+  methods: {
+    handleLogin: function(authInfo) {
+      return this.$store
+        .dispatch("login", authInfo)
+        .then(() => this.$router.push({ path: "/" }))
+        .catch(err => this.throwReject(err));
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-view {
+  width: 320px;
+  margin: auto;
+}
+</style>
+
