17bc0103df41343728b8b89ebb6e03cb3133b1ab
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-11-13 21:11:56 +0900

    .

--- vue-trello/build/dev-server.js
+++ vue-trello/build/dev-server.js
@@ -2,4 +2,26 @@ const bodyParser = require('body-parser');
 
 module.exports = app => {
   app.use(bodyParser.json());
+
+  const users = {
+    'vue@example.com': {
+      password: 'xxxx',
+      userId: 1,
+      token: 'xxxxxxxx'
+    }
+  }
+
+  app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body
+    const user = users[email]
+    if (user) {
+      if (user.password === password) {
+        res.json({ userId: user.userId, token: user.token })
+      } else {
+        res.status(401).json({ message: 'ログインに失敗しました' })
+      }
+    } else {
+      res.status(404).json({ message: 'ユーザが登録されていません' })
+    }
+  })
 };
--- vue-trello/src/components/templates/TrelloLoginView.vue
+++ vue-trello/src/components/templates/TrelloLoginView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="login-view">
         <h1>Trello</h1>
-        <TrelloLoginForm />
+        <TrelloLoginForm :onlogin="handleLogin" />
     </div>
 </template>
 
@@ -19,6 +19,9 @@ export default {
         .dispatch("login", authInfo)
         .then(() => this.$router.push({ path: "/" }))
         .catch(err => this.throwReject(err));
+    },
+    throwReject: function(err) {
+      return Promise.reject(err);
     }
   }
 };
