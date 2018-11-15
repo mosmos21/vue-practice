0b5bd8046b1756465b306e9d0393ef9137111a00
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-11-15 21:10:24 +0900

    .

---
+++ vue-trello/src/ErrorBoundary.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div
+            v-if="error"
+            class="error">
+            <p class="display">予期しないエラーが発生しました</p>
+            <hr>
+            <p class="message">エラーメッセージ: {{ error.mesage }}</p>
+            <p class="info">エラー情報: {{ info }}</p>
+            <p class="stack">エラー詳細: {{ error.stack }}</p>
+        </div>
+        <template c-else>
+            <slot />
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorBoundary",
+  data: function() {
+    return {
+      error: null,
+      info: null
+    };
+  },
+  errorCaptured: function(err, vm, info) {
+    this.error = err;
+    this.info = info;
+  }
+};
+</script>
+
+<style scoped>
+.error {
+  color: red;
+  text-align: left;
+}
+</style>
--- vue-trello/src/main.js
+++ vue-trello/src/main.js
@@ -1,12 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import 'es6-promise//auto'
+import 'es6-promise/auto'
 import App from './App'
+import ErrorBoudary from './ErrorBoundary'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.component(ErrorBoudary.name, ErrorBoudary)
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm: ', vm)
+  console.error('errorHandler info: ', info)
+}
 
 /* eslint-disable no-new */
 new Vue({
